9f37e67fc6a8b0a86d892dfe51ad75c8000426a3
vre2h <hovanisyan.vrezh@gmail.com>
2022-06-11 14:47:32 +0400

    feat(*): add backbone and initial songs

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,20 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
+
+  eleventyConfig.addCollection("song", function(collection) {
+    const coll = collection.getFilteredByTag("song");
+  
+    for(let i = 0; i < coll.length ; i++) {
+      const prevSong = coll[i-1];
+      const nextSong = coll[i + 1];
+  
+      coll[i].data["prevSong"] = prevSong;
+      coll[i].data["nextSong"] = nextSong;
+    }
+  
+    return coll;
+  });
+  
   return {
     passthroughFileCopy: true,
   };
